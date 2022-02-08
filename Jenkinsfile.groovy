96545726c4aa5371b9172052779dd2d15669ed9a
Pakki9669 <99250997+Pakki9669@users.noreply.github.com>
2022-02-08 21:49:40 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,10 @@
 pipeline {
     agent any
-    stage('ensureTools') {
-        def jdkTool= tool 'jdk8'
-        def mvnTool= tool 'Maven 3.3.9'
-        //jdk 'jdk8'
-    }
     stages {
         stage('Compile'){
             steps{
+                def jdkTool= tool 'jdk8'
+                def mvnTool= tool 'Maven 3.3.9'
                 sh script: 'mvn clean -U install compile'
             }
         } 
