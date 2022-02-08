8bfaf8c5baccfefa6e790f98277c3be6876748ae
Pakki9669 <99250997+Pakki9669@users.noreply.github.com>
2022-02-08 21:21:08 +0530

    Changed groovy script

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,15 @@
 pipeline {
-    agent { label 'main' }
+    agent any
+    
+    tools {
+      maven 'Maven_3.5.4'
+    }
     stages {
+        stage('Compile'){
+            steps{
+                sh script: 'mvn clean -U install compile'
+            }
+        } 
         stage('build') {
             steps {
                 echo "Hello World!"
